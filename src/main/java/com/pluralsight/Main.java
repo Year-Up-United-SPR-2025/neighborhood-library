70856f6f9fe68c3cb405b2cfb8141529ceb4f16d
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-22 13:44:35 -0400

    testing

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,13 +3,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+   private static Scanner scanner = new Scanner(System.in);
+    private static Book[] library = getPopulatedLibrary();
+
     public static void main(String[] args) {
 
-        Book[] Libary = getPopulatedLibrary();
+
+
+        ShowScreenHome();
 
     }
 
-    private  static void ShowScreenHome(){
+    private static void ShowScreenHome(){
 
         String  homeScreenPrompt = "Welcome to the library\n" +
                 "Please select an option from the following\n" +
@@ -18,14 +24,33 @@ public class Main {
                 "  0- Exit App\n  " +
                 "(1,2,0):";
 
+        int option;
+
+        do {
+            System.out.print(homeScreenPrompt);
+            option = scanner.nextInt();
+            scanner.nextLine();
+            if (option == 1){
+                ShowScreenAvailableBooks();
+            } else if (option == 2) {
+                ShowScreenCheckedOutBooks();
+            }
+            else if (option ==  0){
+                System.out.println("Exiting the library have a nice day");
+            }
+            else {
+                System.out.println("Not a valid option please try again");
+            }
+        } while (option != 0);
+
     }
 
-    private  static void ShowScreenAvaliableBooks(){
-
+    private  static void ShowScreenAvailableBooks(){
+        System.out.println(".......todo available books menu here");
     }
 
     public static void ShowScreenCheckedOutBooks(){
-
+        System.out.println("...........too checked out books here");
     }
 
 
